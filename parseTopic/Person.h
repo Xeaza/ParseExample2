ce8dc4c4eb579cfa63c8ee2abfc7fbb874c74386
xeaza <taylorwrightsanson@gmail.com>
2014-10-28 10:15:32 -0500

    Added Person class that is a subclass of PFObject

---
+++ parseTopic/Person.h
@@ -0,0 +1,19 @@
+//
+//  Person.h
+//  parseTopicStudentVersion
+//
+//  Created by Taylor Wright-Sanson on 10/28/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Parse/Parse.h>
+
+@interface Person : PFObject <PFSubclassing>
+
+@property NSString *age;
+@property NSNumber *name;
+
+- (BOOL)isTeenager;
+
+@end
