1ef4eabdb074019103797544b52db44b5fdf4515
xeaza <taylorwrightsanson@gmail.com>
2014-10-28 10:27:52 -0500

    Added Dog class

---
+++ Dog.m
@@ -0,0 +1,26 @@
+//
+//  Dog.m
+//  parseTopicStudentVersion
+//
+//  Created by Taylor Wright-Sanson on 10/28/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@dynamic name;
+@dynamic owner;
+
++ (void)load
+{
+    [self registerSubclass];
+}
+
++ (NSString *)parseClassName
+{
+    return @"Dog";
+}
+
+@end
